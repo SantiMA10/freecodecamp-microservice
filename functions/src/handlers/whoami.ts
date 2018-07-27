3dbfd892cc0cc07f844deadfa8df5bcf188330ad
Santiago M.A <santiagomartinagra@gmail.com>
2018-07-27 23:02:46 +0200

    feat(whoami): ip, preferred languages and system info

---
+++ functions/src/handlers/whoami.ts
@@ -0,0 +1,14 @@
+import { Request } from "../../node_modules/@types/express";
+
+export const whoamiHandler = ({ip, headers}: IRequest) => {
+    return {
+        ipaddress: ip,
+        language: headers ? headers['accept-language'] : '',
+        software: headers ? headers['user-agent'] : ''
+    }
+}
+
+interface IRequest {
+    ip?: string
+    headers?: {}
+}
