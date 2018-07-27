266c0d54813b8920184640ba7d779e408a91ca94
Santiago M.A <santiagomartinagra@gmail.com>
2018-07-27 23:03:18 +0200

    feat(functions/index): added whoami route

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,12 +2,18 @@ import * as functions from 'firebase-functions';
 import * as express from 'express';
 import * as cors from 'cors'
 import { timestampHandler } from './handlers/timestamp';
+import { whoamiHandler } from './handlers/whoami';
 
 const app = express()
 app.use(cors({optionsSuccessStatus: 200}))
 
 app.get('/timestamp/:date_string', (req, res) => {
     return res.send(timestampHandler(req.params.date_string))
+})
+
+app.get('/whoami', (req, res) => {
+    console.log(req.headers)
+    return res.send(whoamiHandler(req))
 })
 
 export const api = functions.https.onRequest(app)
