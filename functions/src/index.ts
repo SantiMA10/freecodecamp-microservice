52c0d0837dfc4355fa14152c096604fd09224b06
Santiago M.A <santiagomartinagra@gmail.com>
2018-07-28 12:26:43 +0200

    feat(functions/index): add shorturl new route

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,7 @@ import * as express from 'express';
 import * as cors from 'cors'
 import { timestampHandler } from './handlers/timestamp/timestamp';
 import { whoamiHandler } from './handlers/whoami/whoami';
+import { createShortUrlHandler } from './handlers/urlShortener/createShortUrl';
 
 const app = express()
 app.use(cors({optionsSuccessStatus: 200}))
@@ -13,6 +14,10 @@ app.get('/timestamp/:date_string', (req, res) => {
 
 app.get('/whoami', (req, res) => {
     return res.send(whoamiHandler(req))
+})
+
+app.post('/shorturl/new', (req, res) => {
+    return res.send(createShortUrlHandler({url: req.body.url}))
 })
 
 export const api = functions.https.onRequest(app)
